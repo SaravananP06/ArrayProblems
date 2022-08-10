d96186eea71e0c43dad0119348822a7df25af0de
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-10 19:25:05 +0530

    [Saravanan]: Added program to print elements given in the array

--- src/Day3/BridgeLabz/PrintElements.java
+++ src/Day3/BridgeLabz/PrintElements.java
@@ -1,9 +1,22 @@
 package Day3.BridgeLabz;
 
+/*
+ * This program intializing array and printing the values of array
+ */
 public class PrintElements {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Testing Git");
+		/*
+		 * Initialize array  
+		 */
+        int[] arr = new int[] {1, 2, 3, 4, 5};  
+        System.out.println("Elements of given array: ");  
+        /*
+         * Loop through the array by incrementing value of i  
+         */
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.println(arr[i] + " ");  
+        }  
 
 	}
 
