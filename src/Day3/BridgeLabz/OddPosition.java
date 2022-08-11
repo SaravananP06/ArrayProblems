7de357c68a6da369bf48c7211b783130fe1430e3
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-11 13:00:38 +0530

    [Saravanan]: Added program to print the elements in odd position of the array

---
+++ src/Day3/BridgeLabz/OddPosition.java
@@ -0,0 +1,22 @@
+package Day3.BridgeLabz;
+/*
+ * This will print the elements in the odd position of the array
+ */
+public class OddPosition {
+
+	public static void main(String[] args) {
+		/*
+		 * Initialize array  
+		 */
+	       int [] arr = new int [] {1, 2, 3, 4, 5, 6, 7,};  
+	        System.out.println("Elements of given array present on odd position: ");  
+	        /*
+	         * Loop through the array by incrementing value of i by 2  
+	         */
+	        for (int i = 0; i < arr.length; i = i+2) {  
+	            System.out.println(arr[i]);  
+	        }  
+
+	}
+
+}
